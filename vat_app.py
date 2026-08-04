c5373539a8091eccaa004afbcca6af4e4d6c4528
412-14-kongpop <54321@yupparaj.ac.th>
2026-08-04 10:45:10 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
 vat = price * 0.07
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
@@ -5,5 +6,6 @@ st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.
 import streamlit as st
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 net_price = price - vat
+st.divider()
 st.write("นายก้องภพ มีมาก เลขที่ 14  ม.4/12")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
