5a5b3ed53080f5e6b9202ae1ef909846fbb14a71
412-14-kongpop <54321@yupparaj.ac.th>
2026-09-01 10:29:51 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -44,7 +44,7 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ตรวจข้อ 3
-  if u_ans3 == "Pen":
+   if u_ans3 == "Pen":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
